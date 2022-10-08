2d01589c274e4dd6ddbe32bac579b995c5b765d2
Goshen Jimenez <gjimenez@abcidee.com>
2022-10-08 11:03:32 +0800

    Create Models

---
+++ GoshenJimenez.EFCoreForRazorPages/EFCoreForRazorPages/Infrastructure/Domain/Models/Role.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace EFCoreForRazorPages.Infrastructure.Domain.Models
+{
+    public class Role
+    {
+        public Guid? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Abbreviation { get; set; }
+    }
+}

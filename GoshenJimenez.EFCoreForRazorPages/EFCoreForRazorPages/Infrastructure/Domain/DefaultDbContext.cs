6ad6272bda89b7c4968a151c4692e2d436ae53d4
Goshen Jimenez <gjimenez@abcidee.com>
2022-10-08 11:28:51 +0800

    Adding data to the database from the database initializer

--- GoshenJimenez.EFCoreForRazorPages/EFCoreForRazorPages/Infrastructure/Domain/DefaultDbContext.cs
+++ GoshenJimenez.EFCoreForRazorPages/EFCoreForRazorPages/Infrastructure/Domain/DefaultDbContext.cs
@@ -12,7 +12,34 @@ namespace EFCoreForRazorPages.Infrastructure.Domain
         public DbSet<Role> Roles { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
- 
+            List<Role> roles = new List<Role>();
+
+            roles.Add(new Role()
+            {
+                Id = Guid.Parse("00965ecf-acae-46fe-8775-d7834b07fd96"),
+                Name = "Staff",
+                Description = "People who work in school but not teaching",
+                Abbreviation = "Stf"
+            });
+
+            roles.Add(new Role()
+            {
+                Id = Guid.Parse("7ce68d5c-5b65-495a-8a63-14aeb48da87d"),
+                Name = "Faculty",
+                Description = "People who teach in school",
+                Abbreviation = "Fct"
+            });
+
+
+            roles.Add(new Role()
+            {
+                Id = Guid.Parse("1fb7085a-762f-440c-87de-59f75f85e935"),
+                Name = "Student",
+                Description = "People who learn in school",
+                Abbreviation = "Std"
+            });
+
+            modelBuilder.Entity<Role>().HasData(roles);
         }
 
     }
